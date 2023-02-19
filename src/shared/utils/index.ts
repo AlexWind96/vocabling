d7540cbe8b20848810b88ffbc40ee45abeb03bf9
Alex Pochebut <alexpochebut96@gmail.com>
2023-02-19 18:55:26 +0300

    feat(shared): add utils

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -5,6 +5,10 @@ export const arrayToString = (array: string[]): string => {
   return array.join(', ')
 }
 
+export const isLastItem = (index: number, array: any[]) => {
+  return array.length - 1 === index
+}
+
 export const getDate = (date: Date | Timestamp) => {
   if (date instanceof Timestamp) {
     return date.toDate()
@@ -21,4 +25,8 @@ export function lazyImport<
   return Object.create({
     [name]: React.lazy(() => factory().then((module) => ({ default: module[name] }))),
   })
+}
+
+export const getPercent = (smaller: number, bigger: number) => {
+  return Math.round((smaller / bigger) * 100)
 }
