bbf91f4d26ba4871d4d224f2d00fab3de26baffd
Alex Pochebut <alexpochebut96@gmail.com>
2023-02-19 22:16:00 +0300

    feat(widgets): add learn card widget

---
+++ src/widgets/simple-cards-stack/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Stack, Text } from '@mantine/core'
+import { PaginationArgs } from '@/api'
+import { SimpleCard, useCards } from '@/entities/card'
+import { DeleteCard } from '@/features/card'
+import { LoadingScreen } from '@/shared/ui'
+
+type CardsProps = {
+  moduleId?: string
+  paginationArgs?: PaginationArgs
+}
+
+export const SimpleCardsStack = ({ moduleId, paginationArgs }: CardsProps) => {
+  const { data: cards, isLoading } = useCards({ moduleId, paginationArgs })
+
+  if (isLoading) return <LoadingScreen />
+
+  if (!cards) return <Text>No cards</Text>
+
+  return (
+    <Stack>
+      {cards.map((card) => {
+        return <SimpleCard data={card} key={card.id} rightSection={<DeleteCard id={card.id} />} />
+      })}
+    </Stack>
+  )
+}
