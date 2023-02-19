318e9c132912e262b91ee5ba301343934a8a798a
Alex Pochebut <alexpochebut96@gmail.com>
2023-02-19 21:21:57 +0300

    feat(navigation-entity): add navigation entity

---
+++ src/entities/navigation/navbar-links.ts
@@ -0,0 +1,23 @@
+import { Book, Stack2 } from 'tabler-icons-react'
+import { ROLE } from '@/api'
+import { PATH } from '@/shared/config'
+import { INavbarLink } from './types'
+
+export const navbarLinks: INavbarLink[] = [
+  {
+    path: `${PATH.modules}`,
+    roles: [ROLE.User, ROLE.Admin],
+    label: 'Modules',
+    icon: Stack2,
+  },
+  {
+    path: `${PATH.learn}`,
+    roles: [ROLE.User, ROLE.Admin],
+    label: 'Learn',
+    icon: Book,
+  },
+]
+
+export const mapNavbarLinksByRole = (role): INavbarLink[] => {
+  return navbarLinks.filter((route) => route.roles.includes(role))
+}
