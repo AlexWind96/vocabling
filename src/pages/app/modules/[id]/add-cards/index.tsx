3cf18bcdf372d3dd1f58100ef94350c90fd570d3
Alex Pochebut <alexpochebut96@gmail.com>
2023-02-26 18:50:03 +0300

    feat: add scroll to top in add-card page

--- src/pages/app/modules/[id]/add-cards/index.tsx
+++ src/pages/app/modules/[id]/add-cards/index.tsx
@@ -2,26 +2,29 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { Grid, Title } from '@mantine/core'
 import { AddCard } from '@/features/card'
-import { BackAnchor } from '@/shared/ui'
+import { BackAnchor, ScrollToTop } from '@/shared/ui'
 import { SimpleCardsStack } from '@/widgets/simple-cards-stack'
 
 export const AddCardsPage = () => {
   const { id } = useParams()
   return (
-    <Grid gutter={'sm'}>
-      <Grid.Col>
-        <BackAnchor>Back to module</BackAnchor>
-      </Grid.Col>
-      <Grid.Col>
-        <Title>Create cards</Title>
-      </Grid.Col>
-      <Grid.Col>
-        <AddCard
-          previewSection={
-            <SimpleCardsStack moduleId={id as string} paginationArgs={{ first: 3 }} />
-          }
-        />
-      </Grid.Col>
-    </Grid>
+    <>
+      <Grid gutter={'sm'}>
+        <Grid.Col>
+          <BackAnchor>Back to module</BackAnchor>
+        </Grid.Col>
+        <Grid.Col>
+          <Title>Create cards</Title>
+        </Grid.Col>
+        <Grid.Col>
+          <AddCard
+            previewSection={
+              <SimpleCardsStack moduleId={id as string} paginationArgs={{ first: 3 }} />
+            }
+          />
+        </Grid.Col>
+      </Grid>
+      <ScrollToTop />
+    </>
   )
 }
