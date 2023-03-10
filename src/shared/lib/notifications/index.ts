e1a421dc344f20c08fed47b713e464c036be4528
Alex Pochebut <alexpochebut96@gmail.com>
2023-03-10 23:58:20 +0300

    feat(shared): add common Notifications Service

---
+++ src/shared/lib/notifications/index.ts
@@ -0,0 +1,10 @@
+import { showNotification } from '@mantine/notifications'
+import { NotificationProps } from '@mantine/notifications/lib/types'
+
+class NotificationsService {
+  showError = (payload: Omit<NotificationProps, 'color' | 'title'>): void => {
+    showNotification({ color: 'red', title: 'Error', ...payload })
+  }
+}
+
+export const Notifications = new NotificationsService()
