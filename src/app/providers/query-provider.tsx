a758a2289f4a4eae60a66f2757f9207560f591cf
Alex Pochebut <alexpochebut96@gmail.com>
2023-03-11 00:25:52 +0300

    feat(app): add query dev tools

--- src/app/providers/query-provider.tsx
+++ src/app/providers/query-provider.tsx
@@ -1,7 +1,13 @@
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import { queryClient } from '@/shared/lib/react-query'
 
 export function QueryProvider({ children }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} />
+    </QueryClientProvider>
+  )
 }
