648baec457caab5bb74580cff1948d183de9a5fb
Alex Pochebut <alexpochebut96@gmail.com>
2023-03-17 18:51:57 +0300

    doc: add README.md and small fixes

--- src/entities/auth/model/sagas/logout-saga.ts
+++ src/entities/auth/model/sagas/logout-saga.ts
@@ -3,12 +3,14 @@ import { takeEveryPromiseAction } from 'redux-saga-promise-actions/effects'
 import { call, put } from 'redux-saga/effects'
 import { API } from '@/shared/api'
 import { jwtTokenService } from '@/shared/lib/jwt-token-service'
+import { queryClient } from '@/shared/lib/react-query'
 
 export const logout = createPromiseAction('AUTH_LOGOUT')<undefined, any, Error>()
 
 function* worker() {
   yield call(API.auth.logout)
   jwtTokenService.removeTokens()
+  queryClient.clear()
   yield put(logout.success(undefined))
 }
 
