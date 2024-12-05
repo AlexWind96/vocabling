a2d04a5098eb9dbd9f1932a94f7ceb040e94f7a1
Alex Pochebut <alexpochebut96@gmail.com>
2024-12-06 01:04:13 +0300

    feat: new repetition logic; fixes

--- src/app/providers/store-provider/index.tsx
+++ src/app/providers/store-provider/index.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './store'
 
-
 export function StoreProvider({ children }) {
   return (
     <Provider store={store}>
